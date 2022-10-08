2323361d006aa58629946e6f2a3acc65e86cfec3
Gregory <parfeniuk33@gmail.com>
2022-10-09 01:15:24 +0300

    first version

--- src/FindTheOddInt.java
+++ src/FindTheOddInt.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class FindTheOddInt {
+import java.util.Arrays;
+
+public class FindTheOddInt {
+    public static int findIt(final int[] arr) throws NullPointerException{
+        if (arr == null) throw new NullPointerException("arr cannot be null");
+        Arrays.sort(arr);
+        int currentValue = arr[0];
+        int counter = 0;
+        for (int num : arr) {
+            if (num == currentValue) {
+                ++counter;
+            } else {
+                if (counter % 2 == 1) return currentValue;
+                currentValue = num;
+                counter = 1;
+            }
+        }
+        return currentValue;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args){
+        System.out.println(FindTheOddInt.findIt(new int[]{20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5}));
+    }
 }
